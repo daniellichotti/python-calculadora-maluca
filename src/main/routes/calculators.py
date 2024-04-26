5dd66e92249f35001189d1d4e4c02a5a60074bab
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-26 01:24:17 -0300

    Primeira Calculadora - Iniciando Primeira Calculadora

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator_1
 
 calc_route_bp = Blueprint("calc_route", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=['POST'])
 def calculator_1():
-  print(request.json)
+  calc = Calculator_1()
+  calc.calculate(request)
+  
   return jsonify({"success": True}), 200
