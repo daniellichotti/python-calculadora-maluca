8adf651c26b2fe92b860d246d11a15a2fd6f1f25
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-26 20:09:19 -0300

    Logica da Segunda Calculadora - Finalizando Lógica da Segunda Calculadora

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -9,4 +9,7 @@ def test_calculate():
   mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
 
   calculator_2 = Calculator_2()
-  calculator_2.calculate(mock_request)
+  formated_response = calculator_2.calculate(mock_request)
+
+  assert isinstance(formated_response, dict)
+  assert formated_response == {'data': {'Calculator': 2, 'Result': 0.08}}
