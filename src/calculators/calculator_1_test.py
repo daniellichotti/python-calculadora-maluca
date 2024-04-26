16877bf3214cd70096a28e542fe6598c629bf5c9
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-26 01:55:41 -0300

    Testes Unitários - Testando Erros

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,6 @@
 from .calculator_1 import Calculator_1
 from typing import Dict
+from pytest import raises
 
 class MockRequest:
   def __init__(self, body: Dict) -> None:
@@ -19,3 +20,12 @@ def test_calculate():
   #testar assertividade da resposta
   assert response["data"]["Calculator"] == 1
   assert response["data"]["Result"] == 14.25
+
+def test_calculate_with_body_error():
+  mock_request = MockRequest(body={"something": 1})
+  calculator1 = Calculator_1()
+
+  with raises(Exception) as excinfo:
+    response = calculator1.calculate(mock_request)
+  
+  assert str(excinfo.value) == "body mal formatado"
