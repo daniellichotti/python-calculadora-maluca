30bf323f8872f81f0d39c4c72786dd033b7c7695
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-26 01:32:15 -0300

    Primeira Calculadora - Finalizando Primeira Calculadora

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -8,7 +8,12 @@ class Calculator_1():
     splited_number = input_data / 3 
     
     fist_process_result = self.__first_process(splited_number)
-    
+    second_process_reault = self.__second_process(splited_number)
+    calc_result = fist_process_result + second_process_reault + splited_number
+    response = self.__format_response(calc_result)
+    return response
+
+
 
 
   def __validate_body(self, body: Dict) -> float:
@@ -22,3 +27,16 @@ class Calculator_1():
     first_part = (first_number /4) + 7
     second_part = (first_part**2)*0.257
     return second_part
+  
+  def __second_process(self, second_number: float) -> float:
+    first_part = (second_number ** 2.121)
+    second_part = (first_part / 5) + 1
+    return second_part
+  
+  def __format_response(self, calc_result: float) -> Dict:
+    return {
+      "data": {
+        "Calculator": 1,
+        "Result": round(calc_result, 2)
+      }
+    }
