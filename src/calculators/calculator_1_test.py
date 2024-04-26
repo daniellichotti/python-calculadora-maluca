f97ff964666b25065f6cb597f15e208fe6f4293a
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-26 01:49:43 -0300

    Testes Unitários - Primeiro Teste Unitário

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,21 @@
-def test_something():
-  pass
+from .calculator_1 import Calculator_1
+from typing import Dict
 
-def test_something2():
-  pass
+class MockRequest:
+  def __init__(self, body: Dict) -> None:
+    self.json = body
+
+def test_calculate():
+  mock_request = MockRequest(body={"number": 1})
+  calculator1 = Calculator_1()
+
+  response = calculator1.calculate(mock_request)
+  
+  #testar formato da resposta
+  assert "data" in response
+  assert "Calculator" in response["data"]
+  assert "Result" in response["data"]
+
+  #testar assertividade da resposta
+  assert response["data"]["Calculator"] == 1
+  assert response["data"]["Result"] == 14.25
