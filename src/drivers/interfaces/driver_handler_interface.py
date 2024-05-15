97da063edc8c57ec1d67556f2c9637a807cfcefd
Daniel Lichotti <danielclichotti@gmail.com>
2024-05-14 23:49:44 -0300

    Lógica da Terceira Calculadora - Iniciando Lógica da Terceira Calculadora

--- src/drivers/interfaces/driver_handler_interface.py
+++ src/drivers/interfaces/driver_handler_interface.py
@@ -5,5 +5,9 @@ class DriverHandlerInterface(ABC):
   @abstractmethod
   def standard_derivation(self, numbers: List[float]) -> float:
     pass
+  @abstractmethod
+  def variance(self, numbers: List[float]) -> float:
+    pass
+  
     
 
